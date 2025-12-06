ab880a98d776e119ddd818ed5aaa3972c5700822
torteroloFra <francesco.torterolo@itiscuneo.eu>
2025-12-06 09:36:48 +0100

    Adjust wheel speeds and timing for robot movement

--- Es_4_Percorso.py
+++ Es_4_Percorso.py
@@ -2,36 +2,43 @@ from Alphabot import AlphaBot
 import time
 
 robot = AlphaBot()
-robot.setPWMA(41)
-robot.setPWMB(40)
 
+# modifica velocita'  di default per le ruote per risolvere il problema delle ruote storte
+robot.setPWMA(41) # ruota sinistra
+robot.setPWMB(39) # ruota destra
 
+# va avanti fino a superare l'ostacolo
 robot.forward()
-time.sleep(6.2)
+time.sleep(6)
 
 robot.stop()
 time.sleep(0.5)
 
+# gira a sinistra perpendicolarmente alla traccia precedente
 robot.left()
 time.sleep(0.5)
 
 robot.stop()
 time.sleep(0.5)
 
+# va avanti un po' per superare l'ostacolo
 robot.forward()
 time.sleep(1)
 
 robot.stop()
 time.sleep(0.5)
 
+# gira a sinistra per poter arrivare a destinazione
 robot.left()
-time.sleep(0.6)
+time.sleep(0.5)
 
 robot.stop()
 time.sleep(0.5)
 
+# modifica della velocita'  delle ruote nel caso in cui si siano svitate un po' nel pezzo precedente
 robot.setPWMA(42)
-robot.setPWMB(42)
+robot.setPWMB(41)
 
+# va avanti fino alla fine
 robot.forward()
 time.sleep(6.5)
